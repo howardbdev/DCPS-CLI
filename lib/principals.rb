72b2cbb6a4cb6282f9084ad42d55f997524cce65
Lucy Wall <lhwall@gmail.com>
2018-04-27 18:38:32 +0000

    created initialize function for principal class

---
+++ lib/principals.rb
@@ -0,0 +1,12 @@
+class Principal
+  attr_accessor :first_name, :last_name, :school, :email_address
+  
+  def initialize(first_name, last_name, school, email_address)
+    @first_name = first_name
+    @last_name = last_name
+    @school = school 
+    @email_address = email_address
+  end 
+  
+  
+end 
