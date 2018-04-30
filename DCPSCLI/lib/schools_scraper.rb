6a0f544809364f73c4cb143502e0a452670f4c49
Lucy Wall <lhwall@gmail.com>
2018-04-30 17:48:25 +0000

    setting up all schools scraper method before I identify selectors

--- DCPSCLI/lib/schools_scraper.rb
+++ DCPSCLI/lib/schools_scraper.rb
@@ -1,5 +1,5 @@
 require 'open-uri'
-require 'Nokogiri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
@@ -8,12 +8,24 @@ class Scraper
   def self.scrape_all_schools(school_page_url)
     html = open(school_page_url)
     document = Nokogiri::HTML(html)
+    
+    document.css("#find selector") each do |school|
+      temp_hash = Hash.new 
+      temp_hash[:name] = 
+      temp_hash[:url] =
+   
+    schools_hash = []
+    binding.pry
   end 
   
   #method for scraping details about an individual school
   def self.scrape_school_details(school_profile_url)
     html = open(school_profile_url)
     document = Nokogiri::HTML(html)
+    
+    school_profile_hash = []
   end 
   
 end 
+
+Scraper.scrape_all_schools("http://profiles.dcps.dc.gov")
