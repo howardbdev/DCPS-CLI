d4049e4b2b32410d59445d33b6c1f4a01ff66ffb
Lucy Wall <lhwall@gmail.com>
2018-04-30 14:38:21 +0000

    setting up Scraper class for pulling information

---
+++ DCPSCLI/lib/schools_scraper.rb
@@ -0,0 +1,13 @@
+require 'open-uri'
+require 'Nokogiri'
+require 'pry'
+
+class Scraper
+  
+  #method for scraping the names of all the schools from the DCPS page
+  def self.scrape_all_schools
+  end 
+  
+  #method for scraping details about an individual school
+  def self.scrape_school_details
+  end 
