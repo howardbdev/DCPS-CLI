ec5e86699ab2bf52354297050c6a901d69505c21
Lucy Wall <lhwall@gmail.com>
2018-05-01 19:40:16 +0000

    created method to change grade range into array of grades for each school

--- DCPSCLI/lib/schools.rb
+++ DCPSCLI/lib/schools.rb
@@ -9,8 +9,8 @@ class School
     def initialize(school_hash)
        school_hash.each do |key, value| 
          self.instance_variable_set("@#{key}".to_sym, value)
-      end 
-      @grade_range = []
+      end
+      self.turn_grade_range_into_grades
       @@all << self 
     end 
     
@@ -30,13 +30,34 @@ class School
           if principal.url == school.url 
             school.principal = principal
             principal.school = school
-            binding.pry
           end 
         end 
       end 
     end 
    
     def turn_grade_range_into_grades
+      lowest_grade_string = @grades.split("-")[0]
+      highest_grade_string = @grades.split("-")[1]
+      
+      if lowest_grade_string == "PK3"
+        lowest_grade = -2 
+      elsif lowest_grade_string == "PK4"
+        lowest_grade = -1 
+      elsif lowest_grade_string == "K"
+        lowest_grade = 0
+      elsif lowest_grade_string == "Adult" 
+        lowest_grade = 13 
+      else 
+        lowest_grade = lowest_grade_string.scan(/\d+/).join.to_i
+      end 
+      
+      if highest_grade_string == "Adult"
+        highest_grade = 13 
+      else 
+        highest_grade = highest_grade_string.scan(/\d+/).join.to_i
+      end 
+      
+      @grade_range = Array(lowest_grade..highest_grade)
     end 
   
   
@@ -45,3 +66,4 @@ end
 School.create_from_scraper(Scraper.school_test)
 Principal.create_from_scraper(Scraper.principal_test)
 School.match_principals
+
